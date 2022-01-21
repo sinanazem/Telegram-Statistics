34539394d8048ba817f5d8df914c20a0c3f6c3c6
sinanazem <sinanaazem@gmail.com>
2022-01-21 03:59:03 -0500

    add doc string fot methods and add logger info

--- src/chat_statistics/stats.py
+++ src/chat_statistics/stats.py
@@ -10,11 +10,14 @@ from loguru import logger
 from src.data import DATA_DIR
 from wordcloud import WordCloud
 
-
 class ChatStatistics:
     '''Generates chat statistics from a telegram chat json file'''
     
     def __init__(self, chat_json: Union[str, Path]):
+        '''
+        :param chat_json: path to telegram export json file
+        
+        '''
         # Load Chat Data
         logger.info(f"Loading Chat Data from {chat_json}")
         with open(chat_json) as f:
@@ -22,24 +25,32 @@ class ChatStatistics:
             
             self.normalizer = Normalizer()
             # load stopwords
-            
+            logger.info(f"Loading stopwords from {DATA_DIR / 'stopwords.txt'}")
             stop_words = open(str(DATA_DIR / 'stopword.txt')).readlines()
             stop_words = list(map(str.strip,stop_words))
             self.stop_words = list(map(self.normalizer.normalize,stop_words))
        
        
     def generate_word_cloud(self,output_dir: Union[str, Path]):
+        '''Generates a word cloud from a chat data
+        
+        :param output_dir: path to output directory for word cloud image
+        '''
+        
+        logger.info(f"Loading text content...")
         text_content = ''
         for i in self.chat_data['messages']:
             if type(i['text']) is str:
                 tokens = word_tokenize(i['text'])
                 tokens = list(filter(lambda item: item not in self.stop_words,tokens))
                 text_content += f" {' '.join(tokens)}"
-        
+        # normalize, reshape for final word cloud
         text_content = self.normalizer.normalize(text_content)
         self.text_content = arabic_reshaper.reshape(text_content)
         #text_content = get_display(text_content)
-           
+        
+        logger.info(f"generating word cloud")   
+        #generate wordcloud
         wordcloud = WordCloud(
             width=1800,
             height=800,
